7c68d39d58442acadf50ee7e4149d84537f62800
Kaio Tosto <kaiotosto@icloud.com>
2024-10-24 19:31:25 -0300

    Add initial html skeleton

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
@@ -8,6 +10,5 @@ function Book(title, author, pages, read){
     }
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", "not read yet");
-
-console.log(theHobbit.info());
+function addBookToLibrary() {
+}
