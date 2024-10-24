aef75e8c08d2f36bd60bb41305f841535323c8f6
Kaio Tosto <kaiotosto@icloud.com>
2024-10-24 20:46:26 -0300

    Create addBookToLibrary function

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-const myLibrary = [];
+myLibrary = [];
+const library = document.getElementByClass("library");
 
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return(`${this.title} by ${this.author}, ${this.pages}, ${this.read}`)
-    }
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+function displayBookToLibrary() {
+    for (Book in myLibrary) {
+        row = document.createElement('tr');
+
+        title = document.createElement('td');
+        title.class('title');
+        title.innerHtml = `${myLibrary(Book).title}`
+        row.append(title)
+
+        author = document.createElement('td');
+        author.class('author');
+        author.innerHtml = `${myLibrary(Book).author}`
+        row.append(author)
+
+        pages = document.createElement('td');
+        pages.class('pages');
+        pages.innerHtml = `${myLibrary(Book).pages}`
+        row.append(pages)
+
+        read = document.createElement('rd');
+        read.class('read');
+        read.innerHtml = `${myLibrary(Book).read}`
+        row.append(read)
+
+        library.appendChild(row);
+    }
 }
